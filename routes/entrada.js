917867452a272c2ceae61c8f4d0d53e4e60e18da
petrovick <petrovick@users.noreply.github.com>
2015-06-05 00:09:01 -0300

    Update entrada.js

--- routes/entrada.js
+++ routes/entrada.js
@@ -2,7 +2,8 @@ module.exports = function(app) {
 	var controller = app.controllers.entradaController;
 	
 	app.get('/', controller.get.index);
-	app.get('/adicionar', controller.get.adicionar);
+	
+	app.post('/salvar', controller.post.salvar);
 	
 	//app.post('/calcular', controller.post.calcular);
 };
