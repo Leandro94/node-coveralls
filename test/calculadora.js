f2394633b83d9bc8107fe226aaf52921d6ef8399
Petrovick <petrovickg@github.com>
2015-06-11 20:54:50 -0300

    Repalce Should.ne.a.Number with should.be.true

--- test/calculadora.js
+++ test/calculadora.js
@@ -13,51 +13,6 @@ describe('Calculadora', function() {
 				var resultado = controller.utils.calculos.calcular(parametros);
 			});
 			
-			it('Deveria retornar -3 quando for passado 1 e -4.', function() {
-				
-				var mudaraqui = "Adicionado com sucesso.";
-				var descricao = "Foi";
-				var valor = 123;
-				var resultado = controller.utils.calculos.adicionar(descricao, valor);
-				(mudaraqui == resultado).should.be.true;
-			});
-			
-			it('Deveria retornar 3 quando for passado -1 e 4.', function() {
-				
-				var mudaraqui = "Adicionado com sucesso.";
-				var descricao = "Foi";
-				var valor = 123;
-				var resultado = controller.utils.calculos.adicionar(descricao, valor);
-				(mudaraqui == resultado).should.be.true;
-			});
-			
-			it('Deveria retornar -5 quando for passado -1 e -4.', function() {
-				
-				var mudaraqui = "Adicionado com sucesso.";
-				var descricao = "Foi";
-				var valor = 123;
-				var resultado = controller.utils.calculos.adicionar(descricao, valor);
-				(mudaraqui == resultado).should.be.true;
-			});
-		});
-	});
-	
-	describe('Testa se a função calcular funciona.', function() {
-		describe('Somar', function() {
-			it('Deveria retornar 5 quando somar 1 e 4.', function() {
-				var parametros = {
-					descricao: "Foi",
-					valor: 4,
-					operacao: 'adicionar'
-				};
-				var resultado = controller.utils.calculos.calcular(parametros);
-				var mudaraqui = "Adicionado com sucesso.";
-				(mudaraqui == resultado).should.be.true;
-			});
-		});
-		
-		describe('Subtrair', function() {
-			
 		});
 	});
 
