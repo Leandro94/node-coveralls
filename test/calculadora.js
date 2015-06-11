225d4f32e481bfce1fc9658ab7e2dae2eacead63
Petrovick <petrovickg@github.com>
2015-06-11 20:42:24 -0300

    Taking out Should.ne.a.Number

--- test/calculadora.js
+++ test/calculadora.js
@@ -19,8 +19,6 @@ describe('Calculadora', function() {
 				var descricao = "Foi";
 				var valor = 123;
 				var resultado = controller.utils.calculos.adicionar(descricao, valor);
-				
-				;
 				resultado.should.be.equal(mudaraqui);
 			});
 			
@@ -42,95 +40,6 @@ describe('Calculadora', function() {
 				resultado.should.be.equal(mudaraqui);
 			});
 		});
-		
-		describe('Subtrair', function() {
-			it('Deveria retornar 0 quando for passado 2 e 0.', function() {
-				var numero1 = 2;
-				var numero2 = 0;
-				
-				var resultado = controller.utils.calculos.subtrair(numero1, numero2);
-				
-				resultado.should.be.equal(2);
-			});
-		});
-		
-		describe('Multiplicar', function() {
-			it('Deveria retornar 4 quando for passado 1 e 4.', function() {
-				var numero1 = 1;
-				var numero2 = 4;
-				
-				var resultado = controller.utils.calculos.multiplicar(numero1, numero2);
-				
-				resultado.should.be.equal(4);
-			});
-			
-			it('Deveria retornar -4 quando for passado -1 e 4.', function() {
-				var numero1 = -1;
-				var numero2 = 4;
-				
-				var resultado = controller.utils.calculos.multiplicar(numero1, numero2);
-				
-				resultado.should.be.equal(-4);
-			});
-			
-			it('Deveria retornar -4 quando for passado 1 e -4.', function() {
-				var numero1 = 1;
-				var numero2 = -4;
-				
-				var resultado = controller.utils.calculos.multiplicar(numero1, numero2);
-				
-				resultado.should.be.equal(-4);
-			});
-			
-			it('Deveria retornar 4 quando for passado -1 e -4.', function() {
-				var numero1 = -1;
-				var numero2 = -4;
-				
-				var resultado = controller.utils.calculos.multiplicar(numero1, numero2);
-				
-				resultado.should.be.equal(4);
-			});
-		});
-		
-		describe('Dividir', function() {
-			it('Deveria retornar 0.25 quando for passado 1 e 4.', function() {
-				var numero1 = 1;
-				var numero2 = 4;
-				
-				var resultado = controller.utils.calculos.dividir(numero1, numero2);
-				
-				resultado.should.be.equal(0.25);
-			});
-			
-			it('Deveria retornar -0.25 quando for passado -1 e 4.', function() {
-				var numero1 = -1;
-				var numero2 = 4;
-				
-				var resultado = controller.utils.calculos.dividir(numero1, numero2);
-				
-				resultado.should.be.equal(-0.25);
-			});
-			
-			it('Deveria retornar -0.25 quando for passado 1 e -4.', function() {
-				var numero1 = 1;
-				var numero2 = -4;
-				
-				var resultado = controller.utils.calculos.dividir(numero1, numero2);
-				
-				;
-				resultado.should.be.equal(-0.25);
-			});
-			
-			it('Deveria retornar 0.25 quando for passado -1 e -4.', function() {
-				var numero1 = -1;
-				var numero2 = -4;
-				
-				var resultado = controller.utils.calculos.dividir(numero1, numero2);
-				
-				;
-				resultado.should.be.equal(0.25);
-			});
-		});
 	});
 	
 	describe('Testa se a função calcular funciona.', function() {
@@ -148,21 +57,6 @@ describe('Calculadora', function() {
 		
 		describe('Subtrair', function() {
 			
-		});
-		
-		describe('Multiplicar', function() {
-			it('Deveria retornar 4 quando multiplicar 1 por 4.', function() {
-				var parametros = {
-					descricao: "Foi",
-					valor: 4,
-					operacao: 'adicionar'
-					//operacao: 'multiplicar'
-				};
-				
-				var resultado = controller.utils.calculos.calcular(parametros);
-				
-				resultado.should.be.equal("Adicionado com sucesso.");
-			});
 		});
 	});
 
