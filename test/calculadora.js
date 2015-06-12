c5f356a8a25278c39aa5e1336a16b32836316bbf
Petrovick <petrovickg@github.com>
2015-06-11 21:59:35 -0300

    Tests

--- test/calculadora.js
+++ test/calculadora.js
@@ -3,13 +3,12 @@ var controller = require('../controllers/calculadora.js')();
 describe('Calculadora', function() {
 	describe('Testa se as operações-núcleo funcionam corretamente.', function() {
 		describe('Somar', function() {
-			it('Deveria retornar 5 quando for passado 1 e 4.', function() {
+			it('Deveria retornar adicionado com successo.', function() {
 				var parametros = {
 					descricao: "Isso",
 					valor: 1
 				};
-				var mudaraqui = "Adiocionado com sucesso;"
-
+				var mudaraqui = "Adicionado com sucesso.";
 				var resultado = controller.utils.calculos.calcular(parametros);
 				(mudaraqui == resultado).should.be.true;
 			});
