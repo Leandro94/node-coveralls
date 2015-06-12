3266f6859fbdc9b5b23d883f26283b3d0ced031c
Petrovick <petrovickg@github.com>
2015-06-11 21:55:55 -0300

    Tests

--- test/calculadora.js
+++ test/calculadora.js
@@ -11,6 +11,13 @@ describe('Calculadora', function() {
 				var mudaraqui = "Adiocionado com sucesso;"
 
 				var resultado = controller.utils.calculos.calcular(parametros);
+				(mudaraqui == resultado).should.be.true;
+			});
+
+			it('Deveria retornar 5 quando for passado 1 e 4.', function() {
+				var resultado = "a";
+				var resposta = controller.utils.formatarResposta(resultado);
+				(resposta.resultado == resultado).should.be.true;
 			});
 			
 		});
