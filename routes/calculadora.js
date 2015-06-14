5ebb7fb5976c65489e8af10300f6f1b35dc8e59e
Petrovick <petrovickg@github.com>
2015-06-14 19:36:41 -0300

    Routes configure

--- routes/calculadora.js
+++ routes/calculadora.js
@@ -2,14 +2,6 @@ module.exports = function(app) {
 	var controller = app.controllers.calculadora;
 	
 	app.get('/', controller.get.index);
-	app.get('/adicionar', controller.get.adicionar);
-	app.get('/subtrair', controller.get.subtrair);
-	app.get('/multiplicar', controller.get.multiplicar);
-	app.get('/dividir', controller.get.dividir);
 	app.post('/calcular', controller.post.calcular);
 	
-	var entrada = app.controllers.entrada;
-	app.get('/entrada/', entrada.get.index);
-	app.get('/entrada/index', entrada.get.index);
-	app.post('/salvar', entrada.post.salvar);
 };
