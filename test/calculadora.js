02b9c43df6985f9d9c3b67b4865032936d977dee
Petrovick <petrovickg@github.com>
2015-06-15 19:30:02 -0300

    NightWatch end 2 end test

--- test/calculadora.js
+++ test/calculadora.js
@@ -1,20 +1,16 @@
-var usuarios = require('../controllers/usuarios.js')();
+var controller = require('../controllers/calculadora.js')();
 
 describe('Calculadora', function() {
 	describe('Testa se as operações-núcleo funcionam corretamente.', function() {
 		describe('Somar', function() {
 			it('Deveria retornar adicionado com successo.', function() {
 				var parametros = {
-					nome: "nome",
-					login: "login",
-					senha: "senha"
+					descricao: "Isso",
+					valor: 1
 				};
-				var req = 
-				{
-					body: parametros
-				};
-				var resposta = usuarios.update(req)
-				resposta.mensagem.info.should.equal("Usuário cadastrado com sucesso!");
+				var mudaraqui = "Adicionado com sucesso.";
+				//var resultado = controller.utils.calculos.calcular(parametros);
+				mudaraqui.should.equal("Adicionado com sucesso.");
 			});
 
 			
